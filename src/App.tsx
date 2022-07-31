4c74c791530caa9ce0db4018143ee7be45ac8c63
Aimal Amiri <meaimal@hotmail.com>
2022-07-31 20:11:40 +0430

    The user can add a task

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,17 @@ import { AddNewItem } from './AddNewItem';
 import { Column } from './Column';
 import { AppContainer } from './styles';
 import { useAppState } from './state/AppStateContext';
+import { addList } from './state/actions';
 
 export const App: FC = () => {
-  const { lists } = useAppState();
+  const { lists, dispatch } = useAppState();
 
   return (
     <AppContainer>
       {lists.map((list) => (
         <Column key={list.id} title={list.title} id={list.id} />
       ))}
-      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
+      <AddNewItem toggleButtonText="+ Add another list" onAdd={(title) => dispatch(addList(title))} />
     </AppContainer>
   );
 };
--- src/Column.tsx
+++ src/Column.tsx
@@ -3,6 +3,7 @@ import { AddNewItem } from './AddNewItem';
 import { ColumnContainer, ColumnTitle } from './styles';
 import { useAppState } from './state/AppStateContext';
 import { Card } from './Card';
+import { addTask } from './state/actions';
 
 type ColumnProps = {
   title: string;
@@ -10,17 +11,21 @@ type ColumnProps = {
 };
 
 export const Column: FC<ColumnProps> = ({ title, id }: ColumnProps) => {
-  const state = useAppState();
+  const { getTasksByListId, dispatch } = useAppState();
 
-  const tasks = state.getTasksByListId(id);
+  const tasks = getTasksByListId(id);
 
   return (
     <ColumnContainer>
       <ColumnTitle>{title}</ColumnTitle>
-        {tasks.map((task) => (
-          <Card title={task.title} id={task.id} key={task.id} />
-        ))}
-      <AddNewItem toggleButtonText="+ Add another task" onAdd={console.log} dark />
+      {tasks.map((task) => (
+        <Card title={task.title} id={task.id} key={task.id} />
+      ))}
+      <AddNewItem
+        toggleButtonText="+ Add another task"
+        onAdd={(title) => dispatch(addTask(title, id))}
+        dark
+      />
     </ColumnContainer>
   );
 };

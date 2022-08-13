89afee4137b5c3db8ac1b58686daa748d3c56249
Aimal Amiri <meaimal@hotmail.com>
2022-08-13 21:15:32 +0430

    Add moveItem function to arrayUtils

--- src/utils/arrayUtils.ts
+++ src/utils/arrayUtils.ts
@@ -5,3 +5,8 @@ type Item = {
 export const findItemIndexById = <TItem extends Item>(items: TItem[], id: string) => {
   return items.findIndex((item: TItem) => item.id === id);
 };
+
+export const moveItem = <TItem>(array: TItem[], from: number, to: number) => {
+  const item = array[from];
+  return insertItemAtIndex(removeItemAtIndex(array, from), to, item);
+}

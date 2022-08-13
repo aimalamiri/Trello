2469209bd107318ed41f0dfa747f839abd835fb1
Aimal Amiri <meaimal@hotmail.com>
2022-08-13 22:37:39 +0430

    Add insertItemAtIndex function to arrayUtils

--- src/utils/arrayUtils.ts
+++ src/utils/arrayUtils.ts
@@ -14,3 +14,7 @@ export const moveItem = <TItem>(array: TItem[], from: number, to: number) => {
 export const removeItemAtIndex = <TItem>(array: TItem[], index: number) => {
   return [...array.slice(0, index), ...array.slice(index + 1)];
 };
+
+export const insertItemAtIndex = <TItem>(array: TItem[], item: TItem, index: number) => {
+  return [...array.slice(0, index), item, ...array.slice(index)];
+};
